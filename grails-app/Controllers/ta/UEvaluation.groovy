324062a371f121bb31e4bf7a3e8c227b21cf5c44
Leo Schettini <ljsa@cin.ufpe.br>
2015-05-01 17:57:18 -0300

    implementacao do controlador de avaliacoes

--- grails-app/Controllers/ta/UEvaluation.groovy
+++ grails-app/Controllers/ta/UEvaluation.groovy
@@ -7,29 +7,39 @@ class UEvaluation {
 
 	// responsavel por contruir a avaluacao
 	EvaluationBuilder builder = new EvaluationBuilder()
-
+	String pageMessage
+	/*
 	def index() {
 		// redirect to list action
 	}
 
 	def list() {
 		// list evaluations
-	}
 
+	}
+	*/
 	def getByTitle(String title) {
 		// look for evaluation by it title
+		Evaluation.find(("from Evaluation as e where e.title=?", [title]))
 	}
 
-	def rippenEvaluation(String title, String questionDescription = null, String questionAnswer = null, String questionAlternative = null) {
-		builder.createEvaluation()
-		builder.setEvaluationTitle(title)
-		int quesitonIndex = builder.addEvaluationQuestion(questionDescription)
-		builder.setQuestionAnswer(questionIndex, questionAnswer)
-		builder.addQuestionAlternative(questionIndex, questionAlternative)
+	def rippenEvaluation(String title, String questionDescription = "", String questionAnswer = "", String questionAlternative = "") {
+		
+		try {
+			builder.createEvaluation()
+			builder.setEvaluationTitle(title)
+			int quesitonIndex = builder.addEvaluationQuestion(questionDescription)
+			builder.setQuestionAnswer(questionIndex, questionAnswer)
+			builder.addQuestionAlternative(questionIndex, questionAlternative)
 
-		Evaluation evaluation = builder.getEvaluation()
-		saveEvaluation(evaluation)
+			Evaluation evaluation = builder.getEvaluation()
+			saveEvaluation(evaluation)
+			
+			pageMessage = "Avaliação registrada."
 
+		} catch (Exception e) {
+			pageMessage = "Ocorreu um erro."
+		}
 	}
 
 	def saveEvaluation(Evaluation evaluation) {
